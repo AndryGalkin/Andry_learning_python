81566800acf2f559d9bee9851c2dcf2becd082d3
Andrew <redisland@yandex.ru>
2021-07-19 22:55:23 +0300

    ready

---
+++ Lesson_3_2.py
@@ -0,0 +1,14 @@
+def thesaurus(in_list):
+    res_dic = {}
+    for x_name in sorted(in_list):
+        first_char = x_name[:1]
+        if first_char in res_dic:
+            res_dic[first_char] += [x_name]
+        else:
+            res_dic[first_char] = [x_name]
+
+    return res_dic
+
+
+names = ["Иван", "Мария", "Петр", "Илья"]
+print(thesaurus(names))
