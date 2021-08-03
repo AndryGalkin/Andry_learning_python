852b318d51d2e7588e8a070436d0536ee07665f0
AndryGalkin <redisland@yandex.ru>
2021-08-03 18:40:47 +0300

    ready

---
+++ Lesson_7_2.py
@@ -0,0 +1,22 @@
+import yaml
+import os
+
+with open('config.yaml') as f:
+    content = yaml.safe_load(f)
+
+print(content)
+
+
+def create_path(values, prefix=''):
+    for directory, paths in values.items():
+        dir_path = os.path.join(prefix, directory)
+        os.makedirs(dir_path, exist_ok=True)
+        if isinstance(paths, dict):
+            create_path(paths, dir_path)
+        else:
+            for i in paths:
+                if isinstance(i, dict):
+                    create_path(i, dir_path)
+                elif isinstance(i, str):
+                    with open(os.path.join(dir_path, f'{i}'), 'w') as _:
+                        pass
